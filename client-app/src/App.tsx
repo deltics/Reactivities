1ea4d91a948334477b0da7fc366e73db57aff379
Jolyon Direnko-Smith <jsmith@deltics.co.nz>
2020-08-02 09:36:37 +1200

    Initial client app retrieving values from Api

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,26 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import axios from 'axios';
 import './App.css';
+import {Header, Icon, List, ListItem} from "semantic-ui-react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  
+  state = {
+      values: []
+  }
+  
+  componentDidMount() {
+    axios.get('http://localhost:5000/api/values')
+        .then((response) => {
+          this.setState({
+            values: response.data
+          })
+        })
+  }
+    
+
+  render() {
+    return (
+        <div className="App">
+            <Header as='h2'>
+                <Icon name='users' />
+                <Header.Content>Reactivities</Header.Content>
+            </Header>
+
+            <List>
+              {this.state.values.map((value: any) => (
+                  <ListItem key={value.id}>{value.name}</ListItem>
+              ))}
+            </List>
+            
+            <p>
+              Edit <code>src/App.tsx</code> and save to reload.
+            </p>
+        </div>
+    )
+  }
 }
 
 export default App;
